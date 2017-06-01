0388a7ba2bc40e656003330519da0580a4ed1c7b
baronkoko <san4o0609@gmail.com>
2017-06-01 18:33:40 +0300

    add popup to reviews page

--- js/common.js
+++ js/common.js
@@ -1,44 +1,15 @@
 $(document).ready(function() {
 	//	tabs ready
 //	$(".tabs").lightTabs();
-	
-	//Попап 
-	//Документація: http://dimsemenov.com/plugins/magnific-popup/documentation.html
-	//	<a class="open-popup-link" href="path-to-image.jpg">Open popup</a>
-//	$('.open-popup-link').magnificPopup({
-//		type: 'image',
-//		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-//		// other options
-//	});
-//	//	<a href="#more-content" class="open-popup-link"></a>	
-//	//	$('.open-popup-link').magnificPopup({
-//	//		type:'inline',
-//	//		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-//	//		// other options
-//	//	});			
-//
-//	//Каруселька
-//	//Документація: http://owlgraphic.com/owlcarousel/
-//	var owl = $(".carousel");
-//	owl.owlCarousel({
-//		items : 3,
-//		autoHeight : true
-//	});
-//	owl.on("mousewheel", ".owl-wrapper", function (e) {
-//		if (e.deltaY > 0) {
-//			owl.trigger("owl.prev");
-//		} else {
-//			owl.trigger("owl.next");
-//		}
-//		e.preventDefault();
-//	});
-//	$(".next_button").click(function() {
-//		owl.trigger("owl.next");
-//	});
-//	$(".prev_button").click(function() {
-//		owl.trigger("owl.prev");
-//	});
-//    
+
+
+	//	<a href="#more-content" class="open-popup-link"></a>	
+		$('.leave-review').magnificPopup({
+			type:'inline',
+			midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+			// other options
+		});			
+
 //    //    Слайдер
 //    //    Документація: http://kenwheeler.github.io/slick/
 //    $('.slider').slick({
