89187630cc51ad6e5552be056c8550b6fe57a93c
Dima-js <dimd98@ukr.net>
2017-06-04 23:02:28 +0300

    responsiv implantology

--- js/common.js
+++ js/common.js
@@ -74,7 +74,38 @@ $(document).ready(function() {
 	$('.filtering').slick({
 		slidesToShow: 5,
 		slidesToScroll: 1,
-		arrows: true
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 
 
